962151c83d4382735faae81c603cc3ac35a9ddc2
Yueqi Liao <yliao@ucsd.edu>
2020-04-29 14:28:06 -0700

    message

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,26 @@
+package edu.ucsd.cs110.temperature;
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return new Celsius((this.getValue()-32)*5/9);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit(this.getValue());
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        String beginning = "" + this.getValue();
+        String ending = " F";
+        return beginning + ending;
+    }
+}
